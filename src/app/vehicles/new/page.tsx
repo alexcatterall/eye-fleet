f3b13483268d1461d20602fea9040a5c8b1ac5e5
alexc <aecatterall@icloud.com>
2024-12-07 20:00:17 +0000

    remove vehicle page

--- src/app/vehicles/new/page.tsx
+++
@@ -1,9 +0,0 @@
-import VehicleForm from '@/components/VehicleForm';
-
-export default function NewVehiclePage() {
-  return (
-    <div className="container mx-auto py-8">
-      <VehicleForm />
-    </div>
-  );
-}
